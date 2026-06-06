23c22e74378c3889b21bb2691aa25f74470687c8
Raúl Benencia <id@rbenencia.name>
2026-06-05 21:06:44 -0300

    Update copyright year to 2018-2026

--- test/integ-test/frontend_smoke.js
+++ test/integ-test/frontend_smoke.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /*
-  Copyright 2018 ThousandEyes Inc.
+  Copyright 2018-2026 ThousandEyes Inc.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
