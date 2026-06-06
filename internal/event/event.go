b0b654751c875868bd8c49867a9dbb0ff9bb49c0
Raúl Benencia <id@rbenencia.name>
2026-06-05 21:06:44 -0300

    Update copyright year to 2018-2026

--- internal/event/event.go
+++ internal/event/event.go
@@ -1,4 +1,4 @@
-// Copyright 2018 ThousandEyes Inc.
+// Copyright 2018-2026 ThousandEyes Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
